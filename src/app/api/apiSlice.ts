18c559146d2769d5afe3cc57f8b40c5577c895b0
Medwebdev <mtakisnani09@gmail.com>
2022-11-04 13:55:05 +0100

    Fixed baseurl rtk query

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -1,6 +1,6 @@
 import{ createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const baseQuery = fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API,
+    baseUrl: process.env.REACT_APP_REST_API,
     credentials: 'include',
 })
 
